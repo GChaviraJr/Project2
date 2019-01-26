f870af7c2a4147710a58c50de1d0c22500139489
Lupe <lupec_se@yahoo.com>
2019-01-26 11:22:43 -0700

    Added rules to eslint

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+require("dotenv").config();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
