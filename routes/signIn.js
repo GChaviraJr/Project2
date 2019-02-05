fb70898df6504f704a9308e59b05113baf0ecf48
Lupe <lupec_se@yahoo.com>
2019-02-04 17:50:56 -0700

    change to the promise of signIn under last func.

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,7 +1,5 @@
 const jwt = require("jsonwebtoken");
 const redis = require("redis");
-const bcrypt = require("bcrypt-nodejs");
-var db = require("../models");
 
 const redisClient = redis.createClient(process.env.REDIS);
 
@@ -13,56 +11,45 @@ const signToken = (username) => {
 const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
 
 const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
   db.User.findAll({
     where: {
       email: email,
       id: id
     }
-  });
-  const { email, id } = user;
-  const token = signToken(email);
-  // db.User.findAll({
-  //   where: {
-  //     email: email,
-  //     id: id
-  //   }
-  // });
-  return setToken(token, id);
-  // .then(() => {
-  //   return { success: "true", userId: id, token, user };
-  // })
-  // .catch(console.log);
+  })
+  return setToken(token, id)
+    .then(() => {
+      return { success: "true", userId: id, token, user };
+    })
+    .catch(console.log);
 };
 
-const handleSignin = (db, req, res) => {
+const handleSignin = (db, bcrypt, req, res) => {
+  const { email, password } = req.body;
   if (!email || !password) {
     return Promise.reject("incorrect form submission");
   }
   return db.User.findAll({
     where: {
-      email: email,
-      id: id
+      email: email
     }
-  });
-  // .then(data => {
-  //   const isValid = bcrypt.compareSync(password, data[0].hash);
-  //   if (isValid) {
-  //     db.User.findAll({
-  //       where: {
-  //         email: email,
-  //         password: password
-  //       }
-  //     });
-  // .then(user => user[0])
-  //     .catch(err => res.status(400).json("unable to get user"))
-  // } else {
-  //   return Promise.reject("wrong credentials");
-  // }
-  // })
-  // .catch(err => err);
-  //   }
-  // });
-};
+  }).then(data => {
+    const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        db.User.findAll({
+          where: {
+            email: email
+          }
+        }).then(user => user[0])
+        .catch(err => res.status(400).json("unable to get user"))
+    } else {
+      return Promise.reject("wrong credentials");
+    }
+    })
+    .catch(err => err);
+  };
 
 const getAuthTokenId = (req, res) => {
   const { authorization } = req.headers;
@@ -70,34 +57,25 @@ const getAuthTokenId = (req, res) => {
     if (err || !reply) {
       return res.status(401).send("Unauthorized");
     }
-    return res.json({ id: reply });
+    return res.json({id: reply});
   });
 };
 
 var signinAuthentication = function signinAuthentication(db, bcrypt) {
   return function(req, res) {
     var authorization = req.headers.authorization;
-    console.log(authorization);
     return authorization
       ? getAuthTokenId(req, res)
       : handleSignin(db, bcrypt, req, res)
         .then(function(data) {
-          console.log(data);
-          const isValid = bcrypt.compareSync(password, data[0].hash);
-            if (isValid) {
-              return createSession(data[0]);
-            } else {
-              // Promise.reject(data)
-              return Promise.reject("wrong credentials");
-            }
+          return data.id && data.email
+            ? createSession(data)
+            : Promise.reject(data);
           })
-        .then(function(session) {
-          console.log(session);
-          // { success: "true", userId: id, token, user }
-          return res.json(session);
-          })
-          .catch(function(err) {
-            return res.status(400).json(err);
+          .then(function(session) {
+            return res.json(session);
+        }).catch(function (err) {
+          return res.status(400).json("Unable to start session");
         });
   };
 };
