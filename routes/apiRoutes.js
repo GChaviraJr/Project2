7210bf33aa65e341f18231052f134368dc2f15d5
Lupe <lupec_se@yahoo.com>
2019-02-06 16:36:48 -0700

    took out firebase admin

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -112,14 +112,4 @@ module.exports = function(app) {
 
 };
 
-// firebase code
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../config/firebaseAdmin.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://brewery-crawl-ccd46.firebaseio.com"
-});
 
