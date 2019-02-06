2d88fa9191e694a7a8eb2f7ff15b3f705c8947d1
Lupe <lupec_se@yahoo.com>
2019-02-05 21:49:31 -0700

    changes for signin to signIn

--- routes/authorization.js
+++ routes/authorization.js
@@ -1,4 +1,4 @@
-const redisClient = require("./signin").redisClient;
+const redisClient = require("./signIn").redisClient;
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
